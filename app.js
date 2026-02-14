b789d9d2d822b9d9c2223ce98ec99f62c18f5997
John Michael Kreski <jkreski@regis.edu>
2026-02-14 23:31:20 +0000

    Changed Name

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 8082;
 const server = http.createServer((req, res) => {
 	  res.statusCode = 200;
 	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Hello World. My name is John Michael The Man Kreski');
+	  res.end('Hello World. My name is JMK');
 });
 
 server.listen(port, hostname, () => {
